11bf87b3261a084d2ccbd783590d7f905c94d8d7
ElBandito56 <denzel.moreno@ucas-edu.net>
2025-09-03 10:49:09 -0600

    string notes

---
+++ practices/madlib.py
@@ -0,0 +1,14 @@
+#Dm 2 madlib
+
+adj= input("give me an adjective.")
+name=input("give me your name.")
+event=input("givr me a crazy event")
+place=input("whats a random place")
+verbing=input("give me a verd that ends with ing.")
+verb=input("give me a verb.")
+
+
+
+
+whole_thing = "hey" + " " + name + " " + "did you hear about the" + " " + adj + " " + event + " " +  "it was really crazy.  reminds me when i went to" + " " + place +"."+ " " + "when i went to" + " " +  place + " " + "it was very fun! I was" +   ' '  + verbing +   ' ' + "everywhere! but tell me whyd you come here? to" + " " + verb + " " + 'wow thats super crazy'
+print(whole_thing)
