59f6a2db3f6f3fd1b1b74144ed2f5bc4a95170e3
ElBandito56 <denzel.moreno@ucas-edu.net>
2025-09-10 10:34:46 -0600

    notes

--- projects/idiotproof.py
+++ projects/idiotproof.py
@@ -1,6 +1,6 @@
 #Dm 2cnd Denzel moreno
 
-name= input("What's your full name? ").title()
+name= input("What's your full name? ").title().strip()
 
 phone=input("whats your number.")
 print("Hey " + name )
