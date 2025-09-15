ca46ebd0353a16c44370aeb2deb641c6306414df
ElBandito56 <denzel.moreno@ucas-edu.net>
2025-09-15 10:39:54 -0600

    crew shares

--- projects/crew_shares.py
+++ projects/crew_shares.py
@@ -1,9 +1,9 @@
 #Dm period 2 crew
 
-total_money = input("how much money did they make?") 
-crew = input("how muvh crew member are there?")
-share = total_money/crew 
+total_money = float(input("how much money did they make?"))
+crew = int(input("how muvh crew member are there?"))
+share = (total_money)/(crew + 10)
 captain = share * 7 
 first_mate = share * 3
-total_money = captain  + first_mate - total_money
-print( "the captain gets " + captain + " the first mate has " + first_mate + " and the crew is still owed" + total_money)
+total = captain  + first_mate - total_money
+print(f"the captain gets ${captain:.2f} the first mate has ${first_mate:.2f} and the crew is still owed ${total:.2f}")
