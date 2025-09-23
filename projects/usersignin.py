960b83b2f4e30b3649e3816eda8f83aeea0cbb49
Elbandito56 <denzel.moreno@ucas-edu.net>
2025-09-23 10:45:16 -0600

    got a new seat

---
+++ projects/usersignin.py
@@ -0,0 +1,13 @@
+#Dm 2cnd period user sign in
+
+username_33= input("whats your user name?")
+password_33= input("whats your password?")
+
+if username_33 =="goldentitan" and password_33 == "genovasmova":
+     print("welcome goldentitan")
+elif username_33 == "elbandito56" and password_33 == "ayanami25":
+     print("welcome elbandito56")
+else:
+     print("wrong username or password")
+
+           
