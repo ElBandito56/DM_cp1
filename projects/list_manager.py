7b3e7dc3400037d4e894be35db3f1be5eb80a368
ElBandito56 <denzel.moreno@ucas-edu.net>
2025-09-30 10:48:42 -0600

    progress on list manager

---
+++ projects/list_manager.py
@@ -0,0 +1,14 @@
+#DM class Shopping List Manager
+
+a_list = []
+while True:
+    action = input( "what do you want to do\n 1.add task\n 2.remove task\n 3.show list\n 4.exit\n")    
+    if action == "1":
+        task = input ("what task do you want to add ")
+        a_list.append(task)
+        print ("your task has been added")
+    elif action == "2":
+        task = input("what task do you want removed")
+        if task in a_list
+    elif action == "4":
+        break     
