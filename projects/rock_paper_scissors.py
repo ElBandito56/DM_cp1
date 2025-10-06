a6e157560ecaaedab53e01d36f715ff65cbb9134
ElBandito56 <denzel.moreno@ucas-edu.net>
2025-10-06 10:51:15 -0600

    progress

--- projects/rock_paper_scissors.py
+++ projects/rock_paper_scissors.py
@@ -1,3 +1,14 @@
 #Dm 2cnd period rock paper sciccors
-num = 2
-while num
+#test = 0
+import random
+user_score =0
+computer_score =0
+while True:
+    player = input('lets play rockpaper sciccors what do you want to play "rock" "paper" or "sciccors" please input at least one' )
+    computer = random.randint(1,3)
+    if player == "paper":
+        if computer == 1:
+            print("you lost! sciccors beats paper")
+            computer_score += 1
+        elif computer 
+
