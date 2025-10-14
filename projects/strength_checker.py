6e38f234912e8583fbb6d227cce71a425f139d0a
ElBandito56 <denzel.moreno@ucas-edu.net>
2025-10-14 10:27:22 -0600

    strengh checker

--- projects/strength_checker.py
+++ projects/strength_checker.py
@@ -2,31 +2,41 @@
 #set up  list of special characters
 special_char =  ("z","!",'@','#','$','%','^','&','*','(',')','_','+','-',"=",'[',']','{','}','|',';',':',',','.','<',',','>','?')
 #set the rtating to the start as 0
+upper = False
+lower = False
+special = False
+length = False
+num = False
 rating= 0
 #have user input their PASSWPRD
 password = input("whats your password")
 #check if the password has at least 8 characters if so add one to the rating
 if len(password) >= 8:
     rating += 1
+    length = True
 #check if password has at least one uppercase  if so add one to the rating
 for i in password:
     if i.isupper():
         rating += 1
+        upper = True
         break
 #check if password has at least one lowercase if so add one to the rating
 for i in password:
     if i.islower():
         rating += 1
+        lower = True
         break
 #check if password has at least one number  if so add one to the rating
 for i in password:
     if i.isnumeric():
         rating +=1
+        num = True
         break
 #check if password has at least one specisl character  if so add one to the rating
 for i in password: 
     if i in special_char:
         rating += 1
+        special = True
         break
     
 #then if they have a 5 display very strong password
@@ -44,3 +54,25 @@ elif rating == 1 or 2:
 #else display they didnt input anything
 else:
     print("thats not a real password")
+#checking  if they have  the requirements and telling the user what they have and what there missing
+if length == False:
+    print ("your password is too short")
+if upper == False:
+    print ("your password needs an uppercase letter")
+if lower == False:
+    print ("your password needs a lowercase letter")
+if num == False:
+    print ("your password needs a number")
+if special == False:
+    print("your password needs a special character")
+if length == True:
+    print ("your password is a good length")
+if upper == True:
+    print ("your password has a uppercase letter")
+if lower == True:
+    print ("your password has a lowercase letter")
+if num == True:
+    print ("your password has a number")
+if special == True:
+    print("your password  has a  special character")
+
