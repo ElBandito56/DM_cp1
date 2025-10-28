db2f52c7550ff4820cb6c77f210028869602fd0a
ElBandito56 <denzel.moreno@ucas-edu.net>
2025-10-28 10:51:10 -0600

    turtle race

---
+++ projects/race_turtles.py
@@ -0,0 +1,14 @@
+#Dm 2cnd turtlle Race
+
+#import libaries
+import random
+import turtle
+#Make starting and finsih line
+#make my 5 turtles
+#make turtle 1 red
+#make turtle 2 purple 
+#make turtle 3 green
+#make turtle 5 brown
+#make turtle hot pink
+#randomize speed for each turtle 
+#display " this (turtle who won) won"
