f337fec99066eff432aeb2fb981cc985928f0f76
ElBandito56 <denzel.moreno@ucas-edu.net>
2025-11-05 10:49:37 -0700

    im going to cry

---
+++ projects/maze_generator.py
@@ -0,0 +1,9 @@
+#Dm maze generator 2cnd 
+#import random and turtle
+import random
+import turtle
+#define and set my variables
+#make function to randomize the maze so that its diffrent everytime using random to randomise the floor and walls position
+#make a function to check if the maze is solveable by checking if it has an opening and a closing    
+#make a function to print said maze in tutrtle 
+  
