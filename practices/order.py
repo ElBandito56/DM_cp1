0bc2a248a721d289174fb605261a2008a04931fd
ElBandito56 <denzel.moreno@ucas-edu.net>
2025-11-10 10:50:49 -0700

    order up

---
+++ practices/order.py
@@ -0,0 +1,5 @@
+#DM 2cmd order up
+#make 3 dictionaries, one for drinks, one for main courses, one for sides, the keys will be the item and the value will be the price
+
+#ask if they would like to order.
+# if they choose to order display the menu and then ask them what they would like to otfer
