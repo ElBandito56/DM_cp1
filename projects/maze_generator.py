5c4464661b7d6379e3f4a062de7cacdfca4280f6
ElBandito56 <denzel.moreno@ucas-edu.net>
2025-11-11 10:50:38 -0700

    maze

--- projects/maze_generator.py
+++ projects/maze_generator.py
@@ -4,6 +4,28 @@ import random
 import turtle
 #define and set my variables
 #make function to randomize the maze so that its diffrent everytime using random to randomise the floor and walls position
+
+
+
+def add():
+        rows = [
+        [],
+        [],
+        [],
+        [],
+        [],
+        []
+    ]
+
+        collums= [
+        [],
+        [],
+        [],
+        [],
+        [],
+        []
+    ]
+
+
 #make a function to check if the maze is solveable by checking if it has an opening and a closing    
 #make a function to print said maze in tutrtle 
-  
