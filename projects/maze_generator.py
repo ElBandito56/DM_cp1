c225d598dd85949aa360ed603bfba9d4005340d1
ElBandito56 <denzel.moreno@ucas-edu.net>
2025-11-12 10:51:01 -0700

    maze

--- projects/maze_generator.py
+++ projects/maze_generator.py
@@ -7,8 +7,8 @@ import turtle
 
 
 
-def add():
-        rows = [
+def generatemaze():
+    rows = [
         [],
         [],
         [],
@@ -17,7 +17,7 @@ def add():
         []
     ]
 
-        collums= [
+    collums= [
         [],
         [],
         [],
@@ -25,7 +25,31 @@ def add():
         [],
         []
     ]
+    for row in rows:
+       for i in range(6):
+           row.append(random.randint(0,1))
+    for collum in collums:
+       for i in range(6):
+           row.append(random.randint(0,1))
+    return rows,collums
+    
 
+def draw_maze(grid):
+    turtle.speed(0)
+turtle.penup()
+turtle.pensize(3)
+turtle.goto(x = 0, y = 0)
+turtle.pendown()
+for i in range(4):
+    turtle.forward(60)
+    if turtle.ycor() == 360 or turtle.ycor == 0:
+        turtle.penup()
+        turtle.forward(60)
+        turtle.pendown()
+        turtle.forward(240)
+        turtle.right(90)   
+turtle.penup()
+turtle.done
 
 #make a function to check if the maze is solveable by checking if it has an opening and a closing    
 #make a function to print said maze in tutrtle 
