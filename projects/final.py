81fe2b404f7ad9c3522f76d5f2be4da3be3d1c06
ElBandito56 <denzel.moreno@ucas-edu.net>
2025-12-11 10:48:06 -0700

    final

--- projects/final.py
+++ projects/final.py
@@ -1,6 +1,49 @@
+import random 
+Hp = 150
+Ap = 50
+Speed =100
+Terror = 100
+crit = random.randint(1,5)
 
-start_game = input("welcome to Big scary dream... input (1) when you are ready to begin...")
-if start_game == "1":
-    print("After a long day of trick or treating and watching scary movies your mom told you not to watch, you rest your eyes with a stomach full of candy. But as you slumber in your tiny little bed Suddenly your tiny  are arms jolted, you wake up screaming, pulled by a mysterious dark force as you're thrusted into the main hall, you looked up and saw its john shadow from the horror movie! You pick up a nightlight and get prepared to brawl")
-else:
-    print("you cant do that")
+
+
+
+
+while True:
+
+    start_game = input("welcome to Big scary dream... input (1) when you are ready to begin...")
+    if start_game == "1":
+     print("After a long day of trick or treating and watching scary movies your mom told you not to watch, you rest your eyes with a stomach full of candy. But as you slumber in your tiny little bed Suddenly your tiny  are arms jolted, you wake up screaming, pulled by a mysterious dark force as you're thrusted into the main hall, you looked up and saw its john shadow from the horror movie! You pick up a nightlight and get prepared to brawl" )
+     break
+
+
+
+
+
+
+    else:
+        print("Please input (1) when ready")
+
+
+
+
+def add(monhp,mondio,monap1,monspeed,monap2,monap3):
+   f"{mondio} it stares at you ready to brawl."
+   whogoesfirst = random.randint(1,2)
+   if whogoesfirst == 1:
+      input("check inventory(1)\n attack")
+   elif whogoesfirst == 2:
+      Matk = random.randint(1,2,3)
+      if Matk == 1:
+         monap1 - Hp
+      elif Matk == 2:
+         
+        
+         
+
+
+    
+     
+    
+
+
